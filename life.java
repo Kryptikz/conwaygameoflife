df939219181da7f025e8d982d032254083efa365
Tristan <tblonswa@syr.edu>
2018-04-26 22:16:31 -0400

    Added comments and info during program run

--- life.java
+++ life.java
@@ -1,5 +1,6 @@
 public class life {
     public static void main(String[] args) {
+        int frametime = 100;
         int[][] board = new int[55][100];
         //board[25][25]=1;
         //board[26][26]=1;
@@ -48,7 +49,7 @@ public class life {
                 for (int c=0;c<board[0].length;c++) {
                     temp[r][c]=board[r][c];
                 }
-            }
+            } //copy to temp board, temp board is used for neighbor computation
             for(int r=1;r<board.length-1;r++) {
                 for(int c=1;c<board[0].length-1;c++) {
                     if (board[r][c] == 1) {
@@ -57,46 +58,48 @@ public class life {
                             for (int cc=c-1;cc<=c+1;cc++) {
                                 total+=temp[rr][cc];
                             }
-                        }
+                        } //gets amount of neighbors
                         total--;
                         if (total<2) {
                             board[r][c]=0;
-                        }
+                        } //kill if too few neighbors
                         if (total==2||total==3) {
                             board[r][c]=1;
-                        }
+                        } //leave alive if good amt of neighbors
                         if (total>3) {
                             board[r][c]=0;
-                        }
+                        } //kill if too many neighbors
                     } else if(board[r][c]==0) {
                         int total = 0;
                         for (int rr=r-1;rr<=r+1;rr++) {
                             for (int cc=c-1 ;cc<=c+1;cc++) {
                                 total+=temp[rr][cc];
                             }
-                        }
+                        } //amt of neigbors
                         if (total==3) {
                             board[r][c]=1;
-                        }
+                        } //make alive if perfect amt of neighbors
                     }
                 }
             }
+            System.out.println("FRAME: " + i+1);
+            System.out.println("FRAME TIME: " + frametime);
             for(int r=0;r<board.length;r++) {
                 for(int c=0;c<board[0].length;c++) {
                     if (board[r][c]==0) {
                         System.out.print("  ");
                     } else {
-                        System.out.print("⏺ ");
+                        System.out.print("O ");
                     }
                 }
                 System.out.println();
-            }
+            } //FRAME DRAW
             try {
-                Thread.sleep(60);
+                Thread.sleep(frametime);
             } catch (Exception e) {
                 e.printStackTrace();
-            }
-            System.out.print('\u000C');
+            } //WAIT
+            System.out.print('\u000C'); //CLEAR BLUEJ SCREEN
         }
     }
 }
